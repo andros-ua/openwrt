4f4e6c85b8d6da19a82b4ae9d964b05d09910966
andros-ua <46114524+andros-ua@users.noreply.github.com>
2025-06-13 10:28:53 +0300

    Update filogic.mk
    Cosmetic change in device model name.
    
    Signed-off-by: andros-ua <46114524+andros-ua@users.noreply.github.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -559,7 +559,7 @@ TARGET_DEVICES += cetron_ct3003
 
 define Device/clt_r30b1-112m
   DEVICE_VENDOR := CreatLentem
-  DEVICE_MODEL := CLT R30B1 (112m ubi)
+  DEVICE_MODEL := CLT-R30B1 (112m ubi)
   DEVICE_DTS := mt7981b-clt-r30b1-112m
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-mt76
@@ -577,7 +577,7 @@ TARGET_DEVICES += clt_r30b1-112m
 
 define Device/clt_r30b1
   DEVICE_VENDOR := CreatLentem
-  DEVICE_MODEL := CLT R30B1
+  DEVICE_MODEL := CLT-R30B1
   DEVICE_DTS := mt7981b-clt-r30b1
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-mt76
