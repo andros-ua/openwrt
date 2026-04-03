b2ac5bde516e0b92dd87e9fcae61a1824ca19f85
Andrii Kuiukoff <andros.ua@gmail.com>
2026-04-03 18:42:38 +0300

    mediatek: comfast cf-wr632ax: add UBI layout
    This commit introduces OpenWrt U-Boot UBI layout support
    for the COMFAST CF-WR632AX, enabling:
    - Fully-featured U-Boot
    - Effective recovery mechanisms
    - Prolonged device lifetime by allocating most of the flash
      to UBI (which takes care of wear-leveling)
    - Maximum available storage space for OpenWrt
    - Unified firmware across different device variants
    
    OpenWrt U-Boot UBI flash instructions
    -------------------------------------
    
    Back up critical data
    ---------------------
    1. While the device is running OpenWrt with stock MTD partitions:
       mtd dump BL2 > /tmp/BL2.bin
       mtd dump u-boot-env > /tmp/u-boot-env.bin
       mtd dump Factory > /tmp/Factory.bin
       mtd dump FIP > /tmp/FIP.bin
    2. Copy the backup files to your PC via SCP.
    
    Using the installer image
    -------------------------
    1. Download openwrt*comfast_cf-wr632ax-ubi-initramfs-install.itb
       from https://github.com/andros-ua/owrt-misc/blob/main/cf-wr632ax/ubi/
    2. Flash openwrt*comfast_cf-wr632ax-ubi-initramfs-install.itb
       via sysupgrade.
    3. Copy files to /tmp on the device via SCP:
       Factory.bin
       openwrt*comfast_cf-wr632ax-ubi-preloader.bin
       openwrt*comfast_cf-wr632ax-ubi-bl31-uboot.fip
       openwrt*comfast_cf-wr632ax-ubi-initramfs-recovery.itb
       openwrt*comfast_cf-wr632ax-ubi-squashfs-sysupgrade.itb
    4. Connect via SSH/UART.
    5. Prepare the factory file:
       dd if=/tmp/Factory.bin of=/tmp/factory bs=65536 count=1
    6. Prepare UBI:
       mtd write /tmp/*preloader.bin BL2
       ubidetach -p /dev/mtd1 ; ubiformat -y /dev/mtd1 ; ubiattach -p /dev/mtd1
       ubimkvol /dev/ubi0 -n 0 -t static -s $(du -b /tmp/*uboot.fip | awk '{print $1}') -N fip
       ubiupdatevol /dev/ubi0_0 /tmp/*uboot.fip
       ubimkvol /dev/ubi0 -n 1 -t static -s 65536 -N factory
       ubiupdatevol /dev/ubi0_1 /tmp/factory
       ubimkvol /dev/ubi0 -n 2 -s 126976 -N ubootenv
       ubimkvol /dev/ubi0 -n 3 -s 126976 -N ubootenv2
       ubimkvol /dev/ubi0 -n 4 -s $(du -b /tmp/*recovery.itb | awk '{print $1}') -N recovery
       ubiupdatevol /dev/ubi0_4 /tmp/*recovery.itb
    7. Install the system:
       sysupgrade /tmp/*sysupgrade.itb
    
    Return to stock MTD
    -------------------
    1. Flash openwrt*comfast_cf-wr632ax-initramfs-kernel.bin
       via sysupgrade
    2. Copy files to /tmp on the device via SCP:
       BL2.bin
       u-boot-env.bin
       Factory.bin
       FIP.bin
       openwrt*comfast_cf-wr632ax-squashfs-sysupgrade.bin
    3. Restore stock MTD partitions:
       apk add kmod-mtd-rw
       insmod mtd-rw i_want_a_brick=1
       mtd write /tmp/BL2.bin BL2
       mtd write /tmp/u-boot-env.bin u-boot-env
       mtd write /tmp/Factory.bin Factory
       mtd write /tmp/FIP.bin FIP
    4. Install the system:
       sysupgrade /tmp/*sysupgrade.bin
    
    BL2 and FIP Recovery
    --------------------
    Use mtk_uartboot to recover corrupted BL2 or FIP via UART:
    https://github.com/981213/mtk_uartboot
    
    Stock layout
    ----------------------------------------
    | dev:    size   erasesize  name       |
    | mtd0: 00100000 00020000 "BL2"        |
    | mtd1: 00080000 00020000 "u-boot-env" |
    | mtd2: 00200000 00020000 "Factory"    |
    | mtd3: 00200000 00020000 "FIP"        |
    | mtd4: 07000000 00020000 "ubi"        |
    ----------------------------------------
    
    OpenWrt U-Boot UBI layout
    ----------------------------------
    | dev:    size   erasesize  name |
    | mtd0: 00100000 00020000 "BL2"  |
    | mtd1: 07f00000 00020000 "ubi"  |
    ----------------------------------
    
    Signed-off-by: Andrii Kuiukoff <andros.ua@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -977,9 +977,9 @@ define Device/comfast_cf-wr632ax
 endef
 TARGET_DEVICES += comfast_cf-wr632ax
 
-define Device/comfast_cf-wr632ax-ubootmod
-  DEVICE_VARIANT := (OpenWrt U-Boot layout)
-  DEVICE_DTS := mt7981b-comfast-cf-wr632ax-ubootmod
+define Device/comfast_cf-wr632ax-ubi
+  DEVICE_VARIANT := (UBI)
+  DEVICE_DTS := mt7981b-comfast-cf-wr632ax-ubi
   UBOOTENV_IN_UBI := 1
   IMAGES := sysupgrade.itb
   KERNEL_INITRAMFS_SUFFIX := -recovery.itb
@@ -989,11 +989,11 @@ define Device/comfast_cf-wr632ax-ubootmod
   IMAGE/sysupgrade.itb := append-kernel | \
 	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
   ARTIFACTS := preloader.bin bl31-uboot.fip
-  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr3
+  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ubi-ddr3
   ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot comfast_cf-wr632ax
   $(call Device/comfast_cf-wr632ax-common)
 endef
-TARGET_DEVICES += comfast_cf-wr632ax-ubootmod
+TARGET_DEVICES += comfast_cf-wr632ax-ubi
 
 define Device/confiabits_mt7981
   DEVICE_VENDOR := Confiabits
