e1dc72c22ae9b99cad13dd992b10e2bd8dc22e13
Andrii Kuiukoff <andros.ua@gmail.com>
2026-05-08 00:03:33 +0300

    mediatek: add support for COMFAST CF-WR631AX
    COMFAST CF-WR630AX is a wireless WiFi 6 router.
    
    Specification
    -------------
    - SoC          : MediaTek MT7981BA dual-core ARM Cortex-A53 1.3 GHz
    - RAM          : DDR3 256 MiB
    - Flash        : SPI-NAND 128 MiB
    - WLAN         : MediaTek MT7976CN dual-band WiFi 6
      - 2.4 GHz    : b/g/n/ax, MU-MIMO (2x 5 dBi antennas)
      - 5 GHz      : a/n/ac/ax, MU-MIMO (3x 5 dBi antennas)
    - Ethernet     :
      - LAN x3     : 10/100/1000 Mbps (MediaTek MT7531AE)
      - WAN x1     : 10/100/1000 Mbps (MT7981 internal PHY)
    - UART         : through-hole on PCB
      - assignment : (RX), (TX), (GND), [3.3V]
      - settings   : 115200n8
    - Buttons x2   : Mesh, Reset
    - LEDs x5      : Blue: power , wan, mesh, wlan2G, wlan5G
    - Power        : 12 VDC, 1 A, 2.1*5.5 mm
    
    Flash instructions
    ------------------
    WebUI Method:
    
    1. Install the *squashfs-sysupgrade.bin image using the stock WebUI
       update page.
    2. Press and hold the reset button after reboot
       to wipe the stock config.
    
    Signed-off-by: Andrii Kuiukoff <andros.ua@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -111,6 +111,7 @@ platform_do_upgrade() {
 	bazis,ax3000wm|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
+	comfast,cf-wr631ax-ubi|\
 	comfast,cf-wr632ax-ubootmod|\
 	creatlentem,clt-r30b1-ubi|\
 	comfast,cf-wr633ax-ubi|\
@@ -318,6 +319,7 @@ platform_check_image() {
 	bazis,ax3000wm|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
+	comfast,cf-wr631ax-ubi|\
 	comfast,cf-wr633ax-ubi|\
 	comfast,cf-wr632ax-ubootmod|\
 	creatlentem,clt-r30b1-ubi|\
